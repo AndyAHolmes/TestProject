c5eef4be7b108915fa8ebab3cff33f06e31ca1c3
jonnywebb1 <jonny_webb@live.co.uk>
2023-02-15 14:26:52 +0000

    Added testing around the quote use cases

--- ExerciseApp.Tests/QuoteServiceTests.cs
+++ ExerciseApp.Tests/QuoteServiceTests.cs
@@ -6,12 +6,61 @@ namespace ExerciseApp.Tests
 {
     public class QuoteServiceTests
     {
-        [Fact]
-        public void WhenDetailsAreProvided_AQuoteIsProduced()
+        [Theory]
+        [InlineData("Ford", "Focus", 200)]
+        [InlineData("Audi", "A3", 300)]
+        [InlineData("BMW", "X5", 500)]
+        [InlineData("BMW", "3 Series", 400)]
+        public void WhenDetailsAreProvided_FullyComp_TheCorrectQuoteIsProduced(string make, string model, int quote)
         {
             var qs = new QuoteService();
-            var quoteResult = qs.PerformQuote(new Model.QuoteRequest { DateOfBirth = new DateTime(2000, 05, 01), InsuranceType = Model.InsuranceType.itFullyComprehensive, Make= "Ford", Model="Focus"});
-            Assert.Equal(200, quoteResult);
+
+            var quoteResult = qs.PerformQuote(new Model.QuoteRequest {
+                DateOfBirth = new DateTime(2000, 05, 01),
+                InsuranceType = Model.InsuranceType.itFullyComprehensive,
+                Make=make , Model=model});
+
+            Assert.Equal(quote, quoteResult);
+        }
+
+        [Theory]
+        [InlineData("Ford", "Focus", 180)]
+        [InlineData("Audi", "A3", 300)]
+        [InlineData("BMW", "X5", 510)]
+        [InlineData("BMW", "3 Series", 400)]
+        public void WhenDetailsAreProvided_3PFireAndTheft_TheCorrectQuoteIsProduced(string make, string model, int quote)
+        {
+            var qs = new QuoteService();
+
+            var quoteResult = qs.PerformQuote(new Model.QuoteRequest
+            {
+                DateOfBirth = new DateTime(2000, 05, 01),
+                InsuranceType = Model.InsuranceType.itThirdPartyFireAndTheft,
+                Make = make,
+                Model = model
+            });
+
+            Assert.Equal(quote, quoteResult);
+        }
+
+        [Theory]
+        [InlineData("Ford", "Focus", 180)]
+        [InlineData("Audi", "A3", 250)]
+        [InlineData("BMW", "X5", 300)]
+        [InlineData("BMW", "3 Series", 300)]
+        public void WhenDetailsAreProvided_3Party_TheCorrectQuoteIsProduced(string make, string model, int quote)
+        {
+            var qs = new QuoteService();
+
+            var quoteResult = qs.PerformQuote(new Model.QuoteRequest
+            {
+                DateOfBirth = new DateTime(2000, 05, 01),
+                InsuranceType = Model.InsuranceType.itThirdPartyOnly,
+                Make = make,
+                Model = model
+            });
+
+            Assert.Equal(quote, quoteResult);
         }
 
     }
